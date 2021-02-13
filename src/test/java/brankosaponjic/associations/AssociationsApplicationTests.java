7e1f4502ccadb33ca99c75f8c8dfa4d7a9122a0d
Branko <brankosaponjic76@gmail.com>
2021-02-13 13:30:29 +0100

    Delete Customer

--- src/test/java/brankosaponjic/associations/AssociationsApplicationTests.java
+++ src/test/java/brankosaponjic/associations/AssociationsApplicationTests.java
@@ -50,13 +50,7 @@ class AssociationsApplicationTests {
     }
 
     @Test
-    void testUpdateCustomer() {
-        Optional<Customer> cus = customerRepository.findById(1L);
-        if (cus.isPresent()) {
-            cus.get().setName("James Dean");
-            Set<PhoneNumber> numbers = cus.get().getNumbers();
-            numbers.forEach(p -> p.setType("office"));
-            customerRepository.save(cus.get());
-        }
+    void testDeleteCustomer() {
+      customerRepository.deleteById(3L);
     }
 }
